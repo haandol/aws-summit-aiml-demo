f08f9bce7d3ee7abb63be6d38b17a2fd23a10ba3
haandol <ldg55d@gmail.com>
2023-04-13 00:54:50 +0900

    fix: bug

--- chat/main.py
+++ chat/main.py
@@ -34,8 +34,6 @@ async def startup_event():
     logger.info(f'Loading model: {model_name} with cache_dir: {cache_dir}')
     tokenizer, model = chatbot.setup_model(
         model_name=model_name,
-        load_in_8bit=True,
-        device_map='auto',
         cache_dir=cache_dir,
     )
     logger.info('Model loaded')
