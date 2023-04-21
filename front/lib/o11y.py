0c992626bf516ab2c72ef36d16b4b8ab35abd726
haandol <ldg55d@gmail.com>
2023-04-22 01:42:02 +0900

    set endpoint

--- front/lib/o11y.py
+++ front/lib/o11y.py
@@ -1,17 +1,21 @@
+import os
 from opentelemetry import trace, propagate
 from opentelemetry.propagators.aws import AwsXRayPropagator
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.sampling import ALWAYS_ON
 from opentelemetry.sdk.extension.aws.resource.ecs import AwsEcsResourceDetector
 from opentelemetry.sdk.resources import get_aggregated_resources
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.extension.aws.trace import AwsXRayIdGenerator
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 
-otlp_exporter = OTLPSpanExporter()
+endpoint = os.environ.get('OTEL_EXPORTER_OTLP_ENDPOINT', '0.0.0.0:4317')
+otlp_exporter = OTLPSpanExporter(endpoint=endpoint)
 span_processor = BatchSpanProcessor(otlp_exporter)
 trace.set_tracer_provider(
     TracerProvider(
+        sampler=ALWAYS_ON,
         resource=get_aggregated_resources(
             [ AwsEcsResourceDetector() ]
         ),
