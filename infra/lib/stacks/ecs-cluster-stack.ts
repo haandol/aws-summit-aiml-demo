b68e5d1f3f5620f808dc409da6cc082ce757c754
haandol <ldg55d@gmail.com>
2023-04-22 15:19:39 +0900

    use p3 for quota

--- infra/lib/stacks/ecs-cluster-stack.ts
+++ infra/lib/stacks/ecs-cluster-stack.ts
@@ -64,7 +64,7 @@ export class EcsClusterStack extends Stack {
     );
     const launchTemplate = new ec2.LaunchTemplate(this, 'GpuLaunchTemplate', {
       instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.G4DN,
+        ec2.InstanceClass.P3,
         ec2.InstanceSize.XLARGE2
       ),
       blockDevices: [
