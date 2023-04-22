9d38d53654c4ae527084a5589dcf6f0037617eb1
haandol <ldg55d@gmail.com>
2023-04-22 12:12:02 +0900

    remove scaling by cpu and mem

--- infra/lib/constructs/chatbot-service.ts
+++ infra/lib/constructs/chatbot-service.ts
@@ -118,17 +118,6 @@ export class ChatbotService extends Construct {
       ],
     });
 
-    const scalableTarget = service.autoScaleTaskCount({
-      minCapacity: 1,
-      maxCapacity: 3,
-    });
-    scalableTarget.scaleOnCpuUtilization('CpuScaling', {
-      targetUtilizationPercent: 70,
-    });
-    scalableTarget.scaleOnMemoryUtilization('MemoryScaling', {
-      targetUtilizationPercent: 70,
-    });
-
     return service;
   }
 }
